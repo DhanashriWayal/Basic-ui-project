aaef17ed71d9839ef024fee61cdcc5ade64a708b
Dhanashri Wayal <dhanashriwayal01@gmail.com>
2026-05-30 13:27:28 +0530

    Initial commit - Job Listing UI

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Section1 from './components/Section1/Section1'
+import Section2 from './components/Section2/Section2'
+
+const App = () => {
+
+  const users = [
+    
+    {
+      img: 'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d29ya2luZyUyMHByb2Zlc3Npb25hbHxlbnwwfHwwfHx8MA%3D%3D',
+      intro: '',
+      tag: 'Satisfied'
+    },
+    {
+       img: 'https://plus.unsplash.com/premium_photo-1661769159995-f3af0089875f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d29ya2luZyUyMHByb2Zlc3Npb25hbHxlbnwwfHwwfHx8MA%3D%3D',
+      intro: '',
+      tag: 'Underserved'
+    },
+    {
+       img: 'https://images.unsplash.com/photo-1587614298171-a223667e51c2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHdvcmtpbmclMjBwcm9mZXNzaW9uYWx8ZW58MHx8MHx8fDA%3D',
+      intro: '',
+      tag: 'Underbanked'
+    },
+    {
+       img: 'https://images.unsplash.com/photo-1600275669439-14e40452d20b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d29ya2luZyUyMHByb2Zlc3Npb25hbHxlbnwwfHwwfHx8MA%3D%3D',
+      intro: '',
+      tag: 'Underbanked'
+    },
+    {
+        img:'https://plus.unsplash.com/premium_photo-1661609187240-168f956e08cd?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        intro:'',
+        tag:'Average'
+    },
+  
+  ]
+  return (
+    <div>
+      <Section1 users={users}/>
+      
+    </div>
+  )
+}
+
+export default App
